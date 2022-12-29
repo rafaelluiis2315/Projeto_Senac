c38d11baa11bdba892cc4a468d40a35f95f7847d
rafaelluiis2315 <rafael.barreto@liferay.com>
2022-12-29 14:34:19 -0300

    implementação de listagem de eixos e projetos na pagina principal

--- SENAC_PI/integrifolio/views.py
+++ SENAC_PI/integrifolio/views.py
@@ -1,7 +1,21 @@
 from django.shortcuts import render
+from .models import TURMA, PROJETO, EIXO
+def index(request):
 
-def index(request): 
-    return  render(request, 'index.html') 
+    projetos = PROJETO.objects.all()
+
+    contexto={'projetos': []}
+    
+    for projeto in projetos: 
+        id = projeto.id
+        nome = projeto.nome_projeto
+        capa = projeto.foto_capa
+        eixo = projeto.eixo.nome
+        descricao = projeto.descricao
+        contexto['projetos'].append({'id': id,'nome_projeto':nome,'foto_capa':capa, 'descricao': descricao, 'eixo': eixo }) 
+        
+
+    return  render(request, 'index.html', contexto) 
 
 def details(request):
     return render(request, 'detalhes.hmtl')
